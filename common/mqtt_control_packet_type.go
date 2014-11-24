30854868ad9c90452405ab13f70b5918e8365fb2
yosssi <yoshida.keiji.84@gmail.com>
2014-11-24 21:45:45 +0900

    Update common/mqtt_control_packet_type.go

--- common/mqtt_control_packet_type.go
+++ common/mqtt_control_packet_type.go
@@ -2,21 +2,18 @@ package common
 
 // MQTT Control Packet types
 const (
-	MQTTControlPacketTypeCONNECT     MQTTControlPacketType = 1
-	MQTTControlPacketTypeCONNACK     MQTTControlPacketType = 2
-	MQTTControlPacketTypePUBLISH     MQTTControlPacketType = 3
-	MQTTControlPacketTypePUBACK      MQTTControlPacketType = 4
-	MQTTControlPacketTypePUBREC      MQTTControlPacketType = 5
-	MQTTControlPacketTypePUBREL      MQTTControlPacketType = 6
-	MQTTControlPacketTypePUBCOMP     MQTTControlPacketType = 7
-	MQTTControlPacketTypeSUBSCRIBE   MQTTControlPacketType = 8
-	MQTTControlPacketTypeSUBACK      MQTTControlPacketType = 9
-	MQTTControlPacketTypeUNSUBSCRIBE MQTTControlPacketType = 10
-	MQTTControlPacketTypeUNSUBACK    MQTTControlPacketType = 11
-	MQTTControlPacketTypePINGREQ     MQTTControlPacketType = 12
-	MQTTControlPacketTypePINGRESP    MQTTControlPacketType = 13
-	MQTTControlPacketTypeDISCONNECT  MQTTControlPacketType = 14
+	MQTTControlPacketTypeCONNECT     = 1
+	MQTTControlPacketTypeCONNACK     = 2
+	MQTTControlPacketTypePUBLISH     = 3
+	MQTTControlPacketTypePUBACK      = 4
+	MQTTControlPacketTypePUBREC      = 5
+	MQTTControlPacketTypePUBREL      = 6
+	MQTTControlPacketTypePUBCOMP     = 7
+	MQTTControlPacketTypeSUBSCRIBE   = 8
+	MQTTControlPacketTypeSUBACK      = 9
+	MQTTControlPacketTypeUNSUBSCRIBE = 10
+	MQTTControlPacketTypeUNSUBACK    = 11
+	MQTTControlPacketTypePINGREQ     = 12
+	MQTTControlPacketTypePINGRESP    = 13
+	MQTTControlPacketTypeDISCONNECT  = 14
 )
-
-// MQTTControlPacketType represents an MQTT Control Packet type
-type MQTTControlPacketType byte
