9fbf4a770b60a849d7956b59d6926b5dbd9da035
yosssi <yoshida.keiji.84@gmail.com>
2014-11-27 10:44:10 +0900

    Update client/client.go

--- client/client.go
+++ client/client.go
@@ -1,11 +1,19 @@
 package client
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
 	"sync"
 
 	"github.com/yosssi/gmq/common"
 	"github.com/yosssi/gmq/common/packet"
+)
+
+// Defalut values
+const (
+	defaultErrcBufferSize  = 1024
+	defaultSendcBufferSize = 1024
 )
 
 // Error values
@@ -13,10 +21,16 @@ var ErrAlreadyConnected = errors.New("the Client has already connected to the Se
 
 // Client represents a Client.
 type Client struct {
+	// Errc is a channel handling errors which are sent by the goroutines
+	// which sends or receives MQTT Control Packets.
+	Errc chan error
 	// mu is a reader/writer mutual exclusion lock for the Client.
 	mu sync.RWMutex
 	// networkConnection is a Network Connection.
 	conn *common.Connection
+	// sendc is a channel handling MQTT Control Packets which are sent from
+	// the Client to the Server.
+	sendc chan packet.Packet
 }
 
 // Connect tries to establish a network connection to the Server and
@@ -38,10 +52,67 @@ func (cli *Client) Connect(address string, opts *packet.CONNECTOptions) error {
 	}
 	cli.conn = conn
 
+	// Create a send channel handling MQTT Control Packets and set it to the Client.
+	cli.sendc = make(chan packet.Packet, defaultSendcBufferSize)
+
+	// Launch a goroutine which sends MQTT Control Packets to the Server.
+	go func() {
+		// Create a buffered writer.
+		w := bufio.NewWriter(cli.conn)
+
+		// Send MQTT Control Packets.
+		for p := range cli.sendc {
+			if err := cli.send(w, p); err != nil {
+				cli.Errc <- err
+				continue
+			}
+		}
+	}()
+
+	// Launch a goroutine which receives MQTT Control Packets from the Server.
+	go func() {
+		// Create a buffered reader.
+		r := bufio.NewReader(cli.conn)
+
+		// Receive MQTT Control Packets from the Server.
+		for {
+			if err := cli.receive(r); err != nil {
+				cli.Errc <- err
+				continue
+			}
+		}
+	}()
+
+	// Send the CONNECT Packet to the Server.
+	cli.sendc <- packet.NewCONNECT(opts)
+
+	return nil
+}
+
+// send sends an MQTT Control Packet to the Server.
+func (cli *Client) send(w *bufio.Writer, p packet.Packet) error {
+	if _, err := p.WriteTo(w); err != nil {
+		return err
+	}
+
+	return w.Flush()
+}
+
+// receive receives MQTT Control Packets from the Server
+func (cli *Client) receive(r *bufio.Reader) error {
+	b, err := r.ReadByte()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(b)
+
 	return nil
 }
 
 // New creates and returns a Client.
 func New() *Client {
-	return &Client{}
+	return &Client{
+		Errc: make(chan error, defaultErrcBufferSize),
+	}
 }
--- client/client_test.go
+++ client/client_test.go
@@ -1,8 +1,29 @@
 package client
 
-import "testing"
+import (
+	"bufio"
+	"errors"
+	"io"
+	"io/ioutil"
+	"testing"
+	"time"
+)
 
 const testAddress = "iot.eclipse.org:1883"
+
+type packetErr struct{}
+
+func (w *packetErr) WriteTo(_ io.Writer) (int64, error) {
+	return 0, errTest
+}
+
+type readerErr struct{}
+
+func (r *readerErr) Read(_ []byte) (int, error) {
+	return 0, errTest
+}
+
+var errTest = errors.New("test error")
 
 func TestClient_Connect_addressEmpty(t *testing.T) {
 	if err := New().Connect("", nil); err == nil {
@@ -29,6 +50,74 @@ func TestClient_Connect_errAlreadyConnected(t *testing.T) {
 func TestClient_Connect(t *testing.T) {
 	if err := New().Connect(testAddress, nil); err != nil {
 		t.Errorf("err => %q, want => nil", err)
+	}
+}
+
+func TestClient_Connect_sendErr(t *testing.T) {
+	cli := New()
+
+	if err := cli.Connect(testAddress, nil); err != nil {
+		t.Errorf("err => %q, want => nil", err)
+	}
+
+	cli.sendc <- &packetErr{}
+
+	select {
+	case err := <-cli.Errc:
+		if err != errTest {
+			if err == nil {
+				t.Errorf("err => nil, want => %q", errTest)
+			} else {
+				t.Errorf("err => %q, want => %q", err, errTest)
+			}
+		}
+	case <-time.After(10 * time.Second):
+		t.Errorf("err => nil, want => %q", errTest)
+	}
+}
+
+func TestClient_Connect_receive(t *testing.T) {
+	cli := New()
+
+	if err := cli.Connect(testAddress, nil); err != nil {
+		t.Errorf("err => %q, want => nil", err)
+	}
+
+	cli.conn.Write([]byte{0})
+
+	select {
+	case <-cli.Errc:
+	case <-time.After(10 * time.Second):
+		t.Error("err => nil, want => not nil")
+	}
+
+}
+
+func TestClient_send_err(t *testing.T) {
+	cli := New()
+
+	w := bufio.NewWriter(ioutil.Discard)
+
+	if err := cli.send(w, &packetErr{}); err != errTest {
+		if err == nil {
+			t.Errorf("err => nil, want => %q", errTest)
+		} else {
+			t.Errorf("err => %q, want => %q", err, errTest)
+		}
+	}
+}
+
+func TestClient_receive_err(t *testing.T) {
+	cli := New()
+
+	r := bufio.NewReader(&readerErr{})
+
+	if err := cli.receive(r); err != errTest {
+		if err == nil {
+			t.Errorf("err => nil, want => %q", errTest)
+		} else {
+			t.Errorf("err => %q, want => %q", err, errTest)
+		}
 	}
 }
 
