95f1d9efdc8e95251f2e2957312c0dd427a5bb68
yosssi <yoshida.keiji.84@gmail.com>
2014-11-30 10:06:31 +0900

    Add common/session.go

---
+++ common/session.go
@@ -0,0 +1,10 @@
+package common
+
+// Session represents a Session which is a stateful interaction between a Client and a Server.
+type Session struct {
+}
+
+// NewSession creates and returns a Session.
+func NewSession() *Session {
+	return &Session{}
+}
