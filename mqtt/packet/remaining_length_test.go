d87850507d3359479e0dbd0f62f5d65ca2abc078
yosssi <yoshida.keiji.84@gmail.com>
2014-12-06 21:17:20 +0900

    Update mqtt/packet/remaining_length_test.go

--- mqtt/packet/remaining_length_test.go
+++ mqtt/packet/remaining_length_test.go
@@ -3,5 +3,41 @@ package packet
 import "testing"
 
 func Test_appendRemainingLength(t *testing.T) {
+	testCase := []struct {
+		in  uint32
+		out []byte
+	}{
+		{
+			in:  0xFF000000,
+			out: []byte{255, 0, 0, 0},
+		},
+		{
+			in:  0x00FF0000,
+			out: []byte{255, 0, 0},
+		},
+		{
+			in:  0x0000FF00,
+			out: []byte{255, 0},
+		},
+		{
+			in:  0x000000FF,
+			out: []byte{255},
+		},
+	}
 
+	for _, tc := range testCase {
+		b := appendRemainingLength([]byte{}, tc.in)
+
+		if len(b) != len(tc.out) {
+			t.Errorf("len(b) => %d, want => %d", len(b), len(tc.out))
+			continue
+		}
+
+		for i, bt := range b {
+			if bt != tc.out[i] {
+				t.Errorf("bt => %X, want => %X", bt, tc.out[i])
+				continue
+			}
+		}
+	}
 }
