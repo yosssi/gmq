172c8e174ae41d47e34cebaf8ba10d69c8bc0ddd
yosssi <yoshida.keiji.84@gmail.com>
2014-12-07 19:12:27 +0900

    Update mqtt/qos.go

--- mqtt/qos.go
+++ mqtt/qos.go
@@ -2,7 +2,10 @@ package mqtt
 
 // Values of QoS.
 const (
+	// QoS0 represents "QoS 0: At most once delivery".
 	QoS0 = iota
+	// QoS1 represents "QoS 1: At least once delivery".
 	QoS1
+	// QoS2 represents "QoS 2: Exactly once delivery".
 	QoS2
 )
