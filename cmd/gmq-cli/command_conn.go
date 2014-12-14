d3f9063523768d8aaae87321489872fe3e8618cf
yosssi <yoshida.keiji.84@gmail.com>
2014-12-14 13:59:25 +0900

    Update cmd/gmq-cli/command_conn.go

--- cmd/gmq-cli/command_conn.go
+++ cmd/gmq-cli/command_conn.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"errors"
 	"flag"
-	"fmt"
 	"os"
 	"strconv"
 	"time"
@@ -13,20 +13,25 @@ import (
 
 // Default values
 const (
-	defaultNetwork      = "tcp"
-	defaultHost         = "localhost"
-	defaultPort    uint = 1883
+	defaultNetwork             = "tcp"
+	defaultHost                = "localhost"
+	defaultPort           uint = 1883
+	defaultCONNACKTimeout uint = 30
 )
 
 // Hostname
 var hostname, _ = os.Hostname()
 
+// Error value
+var errCONNACKTimeout = errors.New("the Network Connection was disconnected because the CONNACK Packet was not received within a reasonalbe amount of time")
+
 // commandConn represents a conn command.
 type commandConn struct {
-	ctx         *context
-	network     string
-	address     string
-	connectOpts *packet.CONNECTOptions
+	ctx            *context
+	network        string
+	address        string
+	connackTimeout time.Duration
+	connectOpts    *packet.CONNECTOptions
 }
 
 // run tries to establish a Network Connection to the Server and
@@ -74,6 +79,29 @@ func (cmd *commandConn) run() error {
 		}
 	}()
 
+	// Channel to notify the arrival of the CONNACK Packet
+	var connackc chan struct{}
+
+	// Monitor the arrival of the CONNACK Packet if connackTimeout > 0.
+	if cmd.connackTimeout > 0 {
+		// Create a channel to notify the arrival of the CONNACK Packet.
+		connackc = make(chan struct{})
+
+		go func() {
+			select {
+			case <-connackc:
+			case <-time.After(cmd.connackTimeout * time.Second):
+				// Disconnect the Network Connection.
+				if err := cmd.ctx.disconnect(); err != nil {
+					cmd.ctx.errc <- err
+					return
+				}
+
+				cmd.ctx.errc <- errCONNACKTimeout
+			}
+		}()
+	}
+
 	// Launch a goroutine which handles a Packet received from the Server.
 	go func() {
 		for p := range cmd.ctx.recvc {
@@ -83,7 +111,13 @@ func (cmd *commandConn) run() error {
 				continue
 			}
 
-			fmt.Println(ptype, p)
+			switch ptype {
+			case packet.TypeCONNACK:
+				// Notify the arrival of the CONNACK Packet.
+				if connackc != nil {
+					//connackc <- struct{}{}
+				}
+			}
 		}
 	}()
 
@@ -99,6 +133,11 @@ func newCommandConn(args []string, ctx *context) (*commandConn, error) {
 	network := flg.String("n", defaultNetwork, "network on which the Client connects to the Server")
 	host := flg.String("h", defaultHost, "host name of the Server to connect to")
 	port := flg.Uint("p", defaultPort, "port number of the Server to connect to")
+	connackTimeout := flg.Uint(
+		"act",
+		defaultCONNACKTimeout,
+		"Timeout in seconds for the Client to wait receiving the CONNACK Packet after sending the CONNECT Packet",
+	)
 	clientID := flg.String("i", hostname, "Client identifier for the Client")
 	cleanSession := flg.Bool("c", packet.DefaultCleanSession, "Clean Session")
 	willTopic := flg.String("wt", "", "Will Topic")
@@ -116,9 +155,10 @@ func newCommandConn(args []string, ctx *context) (*commandConn, error) {
 
 	// Create a conn command.
 	cmd := &commandConn{
-		ctx:     ctx,
-		network: *network,
-		address: *host + ":" + strconv.Itoa(int(*port)),
+		ctx:            ctx,
+		network:        *network,
+		address:        *host + ":" + strconv.Itoa(int(*port)),
+		connackTimeout: time.Duration(*connackTimeout),
 		connectOpts: &packet.CONNECTOptions{
 			ClientID:     *clientID,
 			CleanSession: cleanSession,
--- cmd/gmq-cli/context.go
+++ cmd/gmq-cli/context.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"sync"
+
 	"github.com/yosssi/gmq/mqtt/client"
 	"github.com/yosssi/gmq/mqtt/packet"
 )
@@ -15,15 +17,29 @@ const (
 // context represents a context of GMO Client.
 type context struct {
 	cli   *client.Client
+	climu *sync.RWMutex
 	sendc chan packet.Packet
 	recvc chan packet.Packet
 	errc  chan error
+}
+
+// disconnect disconnects the Network Connection.
+func (ctx *context) disconnect() error {
+	ctx.climu.Lock()
+	defer ctx.climu.Unlock()
+
+	if err := ctx.cli.Disconnect(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // newContext creates and returns a context.
 func newContext() *context {
 	return &context{
 		cli:   client.New(nil),
+		climu: new(sync.RWMutex),
 		sendc: make(chan packet.Packet, defaultSendcBufSize),
 		recvc: make(chan packet.Packet, defaultRecvcBufSize),
 		errc:  make(chan error, defaultErrcBufSize),
