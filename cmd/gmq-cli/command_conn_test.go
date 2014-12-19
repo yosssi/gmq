3fdd8360257ff3b8a81844fb3a6663c152d57d4f
yosssi <yoshida.keiji.84@gmail.com>
2014-12-19 23:14:53 +0900

    Update cmd/gmq-cli/command_conn_test.go

--- cmd/gmq-cli/command_conn_test.go
+++ cmd/gmq-cli/command_conn_test.go
@@ -2,6 +2,21 @@ package main
 
 import "testing"
 
+const testAddress = "iot.eclipse.org:1883"
+
+func Test_commandConn_run_err(t *testing.T) {
+	ctx := newContext()
+
+	cmd, err := newCommandConn(nil, ctx)
+	if err != nil {
+		t.Errorf("err => %q, want => nil", err)
+	}
+
+	if err := cmd.run(); err == nil {
+		t.Error("err => nil, want => not nil")
+	}
+}
+
 func Test_commandConn_run(t *testing.T) {
 	ctx := newContext()
 
@@ -10,5 +25,106 @@ func Test_commandConn_run(t *testing.T) {
 		t.Errorf("err => %q, want => nil", err)
 	}
 
-	cmd.run()
+	cmd.network = "tcp"
+	cmd.address = testAddress
+
+	if err := cmd.run(); err != nil {
+		t.Error("err => %q, want => nil", err)
+	}
+}
+
+func Test_commandConn_waitCONNACK_connack(t *testing.T) {
+	ctx := newContext()
+
+	cmd, err := newCommandConn(nil, ctx)
+	if err != nil {
+		t.Errorf("err => %q, want => nil", err)
+	}
+
+	cmd.ctx.wg.Add(1)
+	go cmd.waitCONNACK()
+
+	cmd.ctx.connack <- struct{}{}
+
+	cmd.ctx.wg.Wait()
+}
+
+func Test_commandConn_waitCONNACK_timeout(t *testing.T) {
+	ctx := newContext()
+
+	cmd, err := newCommandConn(nil, ctx)
+	if err != nil {
+		t.Errorf("err => %q, want => nil", err)
+	}
+
+	cmd.connackTimeout = 1
+
+	cmd.ctx.wg.Add(1)
+	go cmd.waitCONNACK()
+
+	cmd.ctx.wg.Wait()
+}
+
+func Test_commandConn_waitCONNACK_timeout_disconnDefault(t *testing.T) {
+	ctx := newContext()
+
+	cmd, err := newCommandConn(nil, ctx)
+	if err != nil {
+		t.Errorf("err => %q, want => nil", err)
+	}
+
+	cmd.connackTimeout = 1
+
+	cmd.ctx.disconn <- struct{}{}
+
+	cmd.ctx.wg.Add(1)
+	go cmd.waitCONNACK()
+
+	cmd.ctx.wg.Wait()
+}
+
+func Test_commandConn_waitCONNACK_connackEnd(t *testing.T) {
+	ctx := newContext()
+
+	cmd, err := newCommandConn(nil, ctx)
+	if err != nil {
+		t.Errorf("err => %q, want => nil", err)
+	}
+
+	cmd.ctx.wg.Add(1)
+	go cmd.waitCONNACK()
+
+	cmd.ctx.connackEnd <- struct{}{}
+
+	cmd.ctx.wg.Wait()
+}
+
+func Test_commandCon_receive_ReceiveErr_disconnecting(t *testing.T) {
+	ctx := newContext()
+
+	cmd, err := newCommandConn(nil, ctx)
+	if err != nil {
+		t.Errorf("err => %q, want => nil", err)
+	}
+
+	cmd.ctx.disconnecting = true
+
+	cmd.ctx.wg.Add(1)
+	go cmd.receive()
+
+	cmd.ctx.wg.Wait()
+}
+
+func Test_commandCon_receive_ReceiveErr(t *testing.T) {
+	ctx := newContext()
+
+	cmd, err := newCommandConn(nil, ctx)
+	if err != nil {
+		t.Errorf("err => %q, want => nil", err)
+	}
+
+	cmd.ctx.wg.Add(1)
+	go cmd.receive()
+
+	cmd.ctx.wg.Wait()
 }
