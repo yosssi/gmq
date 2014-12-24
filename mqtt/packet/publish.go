843b6eab0f3cd13809ef82d7a8f11b30dad57591
yosssi <yoshida.keiji.84@gmail.com>
2014-12-24 18:49:29 +0900

    Add mqtt/packet/publish.go

--- mqtt/packet/publish.go
+++ mqtt/packet/publish.go
@@ -1,1 +1,96 @@
 package packet
+
+import "github.com/yosssi/gmq/mqtt"
+
+// publish represents a PUBLISH Packet.
+type publish struct {
+	base
+	// dup is the DUP flag of the fixed header.
+	dup bool
+	// qos is the QoS of the fixed header.
+	qos byte
+	// retain is the Retain of the fixed header.
+	retain bool
+	// topicName is the Topic Name of the varible header.
+	topicName []byte
+	// packetID is the Packet Identifier of the variable header.
+	packetID uint16
+	// message is the Application Message of the payload.
+	message []byte
+}
+
+// setFixedHeader sets the fixed header to the Packet.
+func (p *publish) setFixedHeader() {
+	// Define the first byte of the fixed header.
+	b := TypePUBLISH << 4
+
+	// Set 1 to the Bit 3 if the DUP flag is true.
+	if p.dup {
+		b |= 0x80
+	}
+
+	// Set the value of the Will QoS to the Bit 2 and 1.
+	b |= p.qos << 1
+
+	// Set 1 to the Bit 0 if the Retain is true.
+	if p.retain {
+		b |= 0x01
+	}
+
+	// Append the first byte to the fixed header.
+	p.fixedHeader = append(p.fixedHeader, b)
+
+	// Append the Remaining Length to the fixed header.
+	p.appendRemainingLength()
+}
+
+// setVariableHeader sets the variable header to the Packet.
+func (p *publish) setVariableHeader() {
+	// Append the Topic Name to the variable header.
+	p.variableHeader = appendLenStr(p.variableHeader, p.topicName)
+
+	if p.qos != mqtt.QoS0 {
+		// Append the Packet Identifier to the variable header.
+		p.variableHeader = append(p.variableHeader, encodeUint16(p.packetID)...)
+	}
+}
+
+// setPayload sets the payload to the Packet.
+func (p *publish) setPayload() {
+	p.payload = p.message
+}
+
+// NewPUBLISH creates and returns a PUBLISH Packet.
+func NewPUBLISH(opts *PUBLISHOptions) (Packet, error) {
+	// Initialize the options.
+	if opts == nil {
+		opts = &PUBLISHOptions{}
+	}
+
+	// Validate the options.
+	if err := opts.validate(); err != nil {
+		return nil, err
+	}
+
+	// Create a PUBLISH Packet.
+	p := &publish{
+		dup:       opts.DUP,
+		qos:       opts.QoS,
+		retain:    opts.Retain,
+		topicName: opts.TopicName,
+		packetID:  opts.PacketID,
+		message:   opts.Message,
+	}
+
+	// Set the variable header to the Packet.
+	p.setVariableHeader()
+
+	// Set the payload to the Packet.
+	p.setPayload()
+
+	// Set the Fixed header to the Packet.
+	p.setFixedHeader()
+
+	// Return the Packet.
+	return p, nil
+}
