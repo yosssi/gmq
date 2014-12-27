cbbff8036683cd14b9b1c92dd2eab3b073b3cae7
yosssi <yoshida.keiji.84@gmail.com>
2014-12-27 22:43:36 +0900

    Update mqtt/packet/pubcomp.go

--- mqtt/packet/pubcomp_test.go
+++ mqtt/packet/pubcomp_test.go
@@ -2,6 +2,49 @@ package packet
 
 import "testing"
 
+func TestPUBCOMP_setFixedHeader(t *testing.T) {
+	p := &PUBCOMP{
+		PacketID: 1,
+	}
+
+	p.setFixedHeader()
+
+	want := []byte{0x70, 0x00}
+
+	if len(want) != len(p.fixedHeader) || want[0] != p.fixedHeader[0] || want[1] != p.fixedHeader[1] {
+		t.Errorf("p.fixedHeader => %v, want => %v", p.fixedHeader, want)
+	}
+}
+
+func TestPUBCOMP_setVariableHeader(t *testing.T) {
+	p := &PUBCOMP{
+		PacketID: 1,
+	}
+
+	p.setVariableHeader()
+
+	want := []byte{0x00, 0x01}
+
+	if len(want) != len(p.variableHeader) || want[0] != p.variableHeader[0] || want[1] != p.variableHeader[1] {
+		t.Errorf("p.variableHeader => %v, want => %v", p.variableHeader, want)
+	}
+}
+
+func TestNewPUBCOMP_optsNil(t *testing.T) {
+	if _, err := NewPUBCOMP(nil); err != ErrInvalidPacketID {
+		invalidError(t, err, ErrInvalidPacketID)
+	}
+}
+
+func TestNewPUBCOMP(t *testing.T) {
+	_, err := NewPUBCOMP(&PUBCOMPOptions{
+		PacketID: 1,
+	})
+	if err != nil {
+		nilErrorExpected(t, err)
+	}
+}
+
 func TestNewPUBCOMPFromBytes_validatePUBCOMPBytesErr(t *testing.T) {
 	if _, err := NewPUBCOMPFromBytes(nil, nil); err != ErrInvalidFixedHeaderLen {
 		invalidError(t, err, ErrInvalidFixedHeaderLen)
@@ -47,6 +90,12 @@ func Test_validatePUBCOMPBytes_ErrInvalidRemainingLength(t *testing.T) {
 func Test_validatePUBCOMPBytes_ErrInvalidVariableHeaderLen(t *testing.T) {
 	if err := validatePUBCOMPBytes([]byte{TypePUBCOMP << 4, 0x02}, nil); err != ErrInvalidVariableHeaderLen {
 		invalidError(t, err, ErrInvalidVariableHeaderLen)
+	}
+}
+
+func Test_validatePUBCOMPBytes_ErrInvalidPacketID(t *testing.T) {
+	if err := validatePUBCOMPBytes([]byte{TypePUBCOMP << 4, 0x02}, []byte{0x00, 0x00}); err != ErrInvalidPacketID {
+		invalidError(t, err, ErrInvalidPacketID)
 	}
 }
 
