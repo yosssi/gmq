0166d1813dd58ad36bda74118420f9f2c86ce619
yosssi <yoshida.keiji.84@gmail.com>
2014-12-28 15:46:54 +0900

    Update mqtt/client/client_test.go

--- mqtt/client/client_test.go
+++ mqtt/client/client_test.go
@@ -1,9 +1,71 @@
 package client
 
 import (
+	"errors"
+	"io"
 	"testing"
 	"time"
+
+	"github.com/yosssi/gmq/mqtt/packet"
 )
+
+var errTest = errors.New("test error")
+
+type packetErr struct{}
+
+func (p *packetErr) WriteTo(w io.Writer) (int64, error) {
+	return 0, errTest
+}
+
+func (p *packetErr) Type() (byte, error) {
+	return 0x00, errTest
+}
+
+func TestClient_validatePacketID_notExist(t *testing.T) {
+	cli := New(nil)
+
+	if err := cli.validatePacketID(map[uint16]packet.Packet{}, 1, packet.TypePUBLISH); err != ErrInvalidPacketID {
+		invalidError(t, err, ErrInvalidPacketID)
+	}
+}
+
+func TestClient_validatePacketID_ptypeErr(t *testing.T) {
+	cli := New(nil)
+
+	if err := cli.validatePacketID(map[uint16]packet.Packet{1: &packetErr{}}, 1, packet.TypePUBLISH); err != errTest {
+		invalidError(t, err, errTest)
+	}
+}
+
+func TestClient_validatePacketID_ErrInvalidPacketID(t *testing.T) {
+	cli := New(nil)
+
+	p, err := packet.NewPUBACK(&packet.PUBACKOptions{
+		PacketID: 1,
+	})
+	if err != nil {
+		nilErrorExpected(t, err)
+	}
+
+	if err := cli.validatePacketID(map[uint16]packet.Packet{1: p}, 1, packet.TypePUBLISH); err != ErrInvalidPacketID {
+		invalidError(t, err, ErrInvalidPacketID)
+	}
+}
+
+func TestClient_validatePacketID(t *testing.T) {
+	cli := New(nil)
+
+	p, err := packet.NewPUBLISH(&packet.PUBLISHOptions{
+		PacketID: 1,
+	})
+	if err != nil {
+		nilErrorExpected(t, err)
+	}
+
+	if err := cli.validatePacketID(map[uint16]packet.Packet{1: p}, 1, packet.TypePUBLISH); err != nil {
+		nilErrorExpected(t, err)
+	}
+}
 
 func TestClient_handleMessage_continue(t *testing.T) {
 	cli := New(nil)
@@ -420,5 +482,13 @@ func Test_match(t *testing.T) {
 		if got := match(tc.in.topicName, tc.in.topicFilter); got != tc.out {
 			t.Errorf("got => %t, want => %t", got, tc.out)
 		}
+	}
+}
+
+func invalidError(t *testing.T, err, want error) {
+	if err == nil {
+		t.Errorf("err => nil, want => %q", want)
+	} else {
+		t.Errorf("err => %q, want => %q", err, want)
 	}
 }
