4bdfa2e7fe795b56823da090a324aee49d2534da
yosssi <yoshida.keiji.84@gmail.com>
2014-12-29 13:09:47 +0900

    Add command_conn_test.go

---
+++ cmd/gmq-cli/command_conn_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/yosssi/gmq/mqtt/client"
+)
+
+func Test_commandConn_run(t *testing.T) {
+	cli := client.New(&client.Options{
+		ErrorHandler: func(_ error) {},
+	})
+
+	defer quit(cli)
+
+	cmd, err := newCommandConn(nil, cli)
+	if err != nil {
+		nilErrorExpected(t, err)
+		return
+	}
+
+	if err := cmd.run(); err == nil {
+		notNilErrorExpected(t)
+	}
+}
+
+func Test_newCommandConn_errCmdArgsParse(t *testing.T) {
+	cli := client.New(&client.Options{
+		ErrorHandler: func(_ error) {},
+	})
+
+	defer quit(cli)
+
+	if _, err := newCommandConn([]string{"-not-exist-flag"}, cli); err != errCmdArgsParse {
+		invalidError(t, err, errCmdArgsParse)
+	}
+}
+
+func Test_newCommandConn_ReadFileErr(t *testing.T) {
+	cli := client.New(&client.Options{
+		ErrorHandler: func(_ error) {},
+	})
+
+	defer quit(cli)
+
+	if _, err := newCommandConn([]string{"-crt", "not_exist_file.crt"}, cli); err == nil {
+		notNilErrorExpected(t)
+	}
+}
+
+func Test_newCommandConn_errParseCrtFailure(t *testing.T) {
+	cli := client.New(&client.Options{
+		ErrorHandler: func(_ error) {},
+	})
+
+	defer quit(cli)
+
+	if _, err := newCommandConn([]string{"-crt", filepath.Join("test", "error.crt")}, cli); err != errParseCrtFailure {
+		invalidError(t, err, errParseCrtFailure)
+	}
+}
+
+func Test_newCommandConn(t *testing.T) {
+	cli := client.New(&client.Options{
+		ErrorHandler: func(_ error) {},
+	})
+
+	defer quit(cli)
+
+	if _, err := newCommandConn([]string{"-crt", filepath.Join("test", "mosquitto.org.crt")}, cli); err != nil {
+		nilErrorExpected(t, err)
+	}
+}
+
+func notNilErrorExpected(t *testing.T) {
+	t.Error("err => nil, want => not nil")
+}
